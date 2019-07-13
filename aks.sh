f813d12ed5a9980e017b44a891f274e74f0950c5
Chris Joakim <christopher.joakim@gmail.com>
2019-07-13 16:05:17 -0400

    Upgraded libs in package.json, added aks.sh

---
+++ aks.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Interact with Azure Key Store (AKS)
+# Chris Joakim, Microsoft, 2019/07/13
+
+# az keyvault secret set \
+#   --vault-name $AZURE_AKV_NAME \
+#   --name $AZURE_ACR_NAME-pull-pwd \
+#   --value $(az ad sp create-for-rbac \
+#                 --name $AZURE_ACR_NAME-pull \
+#                 --scopes $(az acr show --name $AZURE_ACR_NAME --query id --output tsv) \
+#                 --role reader \
+#                 --query password \
+#                 --output tsv)
+
+az keyvault secret set \
+    --vault-name $AZURE_AKV_NAME \
+    --name $AZURE_ACR_NAME-pull-usr \
+    --value $(az ad sp show --id http://$AZURE_ACR_NAME-pull --query appId --output tsv)
+
+echo 'done'
